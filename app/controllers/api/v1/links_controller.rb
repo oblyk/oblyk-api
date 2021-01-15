b3a57733c411366ed3a984fb46e25a7eae7a0085
clucien <chastanlucien@gmail.com>
2021-01-15 13:28:39 +0100

    Amélioration lien

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,9 +3,9 @@
 module Api
   module V1
     class LinksController < ApiController
-      before_action :protected_by_super_admin, only: %i[destroy]
-      before_action :protected_by_session, only: %i[create update]
+      before_action :protected_by_session, only: %i[create update destroy]
       before_action :set_link, only: %i[show update destroy]
+      before_action :protected_by_owner, only: %i[update destroy]
 
       def index
         @links = Link.where(
@@ -56,6 +56,10 @@ module Api
           :url,
           :description
         )
+      end
+
+      def protected_by_owner
+        not_authorized if @current_user.id != @link.user_id
       end
     end
   end
