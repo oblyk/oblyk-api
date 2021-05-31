2d643807ee4007b4fb2228dbfe7c307256758ba1
clucien <chastanlucien@gmail.com>
2021-05-31 20:45:04 +0200

    Ajout d'un environement de staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 server 'next.oblyk.org', user: 'lucien', roles: %w[app web db], port: 1622
 
 set :deploy_to, '/var/www/oblyk/api'
+set :rails_env, 'production'
 
 # Deploy with master branch
 set :branch, 'master'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,7 @@
 server 'next.oblyk.org', user: 'lucien', roles: %w[app web db], port: 1622
 
 set :deploy_to, '/var/www/oblyk-next/api'
+set :rails_env, 'staging'
 
 # Deploy with staging branch
 set :branch, 'staging'
