d978f1d6798f459f9b0c527e8648272c37c7b017
clucien <chastanlucien@gmail.com>
2021-07-20 16:01:36 +0200

    On sort la liste des feedables dans une constante pour l'utiliser ailleurs

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,9 +3,11 @@
 class Feed < ApplicationRecord
   belongs_to :feedable, polymorphic: true
 
+  FEEDABLE_LIST = %w[Crag CragRoute GuideBookPaper GuideBookWeb GuideBookPdf Gym Alert Word Photo Video AscentCragRoute User Article].freeze
+
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_blank: true
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_blank: true
-  validates :feedable_type, inclusion: { in: %w[Crag CragRoute GuideBookPaper GuideBookWeb GuideBookPdf Gym Alert Word Photo Video AscentCragRoute User Article].freeze }
+  validates :feedable_type, inclusion: { in: FEEDABLE_LIST }
 
   def location
     [latitude, longitude]
