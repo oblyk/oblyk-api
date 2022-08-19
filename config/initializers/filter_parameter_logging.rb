c0331a0275c2cd534875eeefe8bd7768186ee19e
clucien <chastanlucien@gmail.com>
2022-08-19 17:45:16 +0200

    Ajout de paramètre à filtrer dans les logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,3 +4,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += %i[password password_confirmation]
+Rails.application.config.filter_parameters += %i[httpapiaccesstoken authorization]
+Rails.application.config.filter_parameters += %i[token ws_token refresh_token]
