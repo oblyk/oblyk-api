ad8345ecac3216b9a6cfd3e8e8e7c4f3af1a301b
clucien <chastanlucien@gmail.com>
2023-01-06 19:13:35 +0100

    Changement du service d'envoie sur les emails types notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -8,7 +8,31 @@ class NotificationMailer < ApplicationMailer
 
     to = %("#{@user.email}" <#{@user.email}>)
     I18n.with_locale(@user.language) do
-      mail(to: to, subject: t('mailer.notification.new_message.title'))
+      subject = t('mailer.notification.new_message.title')
+      if ENV['SEND_EMAIL_WITH'] == 'send_in_blue'
+        html_content = render_to_string('notification_mailer/new_message')
+        @sid_email = SibApiV3Sdk::SendSmtpEmail.new
+        @sid_email.to = [{ email: to }]
+        @sid_email.subject = subject
+        @sid_email.html_content = html_content
+        @sid_email.text_content = ActionView::Base.full_sanitizer.sanitize(html_content)
+        @sid_email.sender = {
+          name: ENV['SEND_IN_BLUE_SENDER_NAME'],
+          email: ENV['SEND_IN_BLUE_SENDER_EMAIL']
+        }
+        @sid_email.reply_to = {
+          email: ENV['SEND_IN_BLUE_REPLY_EMAIL'],
+          name: ENV['SEND_IN_BLUE_SENDER_NAME']
+        }
+
+        begin
+          @send_in_blue.send_transac_email(@sid_email)
+        rescue SibApiV3Sdk::ApiError => e
+          Rails.logger.error "Exception when calling TransactionalEmailsApi -> send_transac_email: #{e}"
+        end
+      else
+        mail(to: to, subject: subject)
+      end
     end
   end
 
@@ -20,7 +44,31 @@ class NotificationMailer < ApplicationMailer
     @follower = params[:follower]
     to = %("#{@user.email}" <#{@user.email}>)
     I18n.with_locale(@user.language) do
-      mail(to: to, subject: t('mailer.notification.request_for_follow_up.title', name: @follower.first_name))
+      subject = t('mailer.notification.request_for_follow_up.title')
+      if ENV['SEND_EMAIL_WITH'] == 'send_in_blue'
+        html_content = render_to_string('notification_mailer/request_for_follow_up')
+        @sid_email = SibApiV3Sdk::SendSmtpEmail.new
+        @sid_email.to = [{ email: to }]
+        @sid_email.subject = subject
+        @sid_email.html_content = html_content
+        @sid_email.text_content = ActionView::Base.full_sanitizer.sanitize(html_content)
+        @sid_email.sender = {
+          name: ENV['SEND_IN_BLUE_SENDER_NAME'],
+          email: ENV['SEND_IN_BLUE_SENDER_EMAIL']
+        }
+        @sid_email.reply_to = {
+          email: ENV['SEND_IN_BLUE_REPLY_EMAIL'],
+          name: ENV['SEND_IN_BLUE_SENDER_NAME']
+        }
+
+        begin
+          @send_in_blue.send_transac_email(@sid_email)
+        rescue SibApiV3Sdk::ApiError => e
+          Rails.logger.error "Exception when calling TransactionalEmailsApi -> send_transac_email: #{e}"
+        end
+      else
+        mail(to: to, subject: subject)
+      end
     end
   end
 
@@ -32,7 +80,31 @@ class NotificationMailer < ApplicationMailer
     @article = params[:article]
     to = %("#{@user.email}" <#{@user.email}>)
     I18n.with_locale(@user.language) do
-      mail(to: to, subject: t('mailer.notification.new_article.title'))
+      subject = t('mailer.notification.new_article.title')
+      if ENV['SEND_EMAIL_WITH'] == 'send_in_blue'
+        html_content = render_to_string('notification_mailer/new_article')
+        @sid_email = SibApiV3Sdk::SendSmtpEmail.new
+        @sid_email.to = [{ email: to }]
+        @sid_email.subject = subject
+        @sid_email.html_content = html_content
+        @sid_email.text_content = ActionView::Base.full_sanitizer.sanitize(html_content)
+        @sid_email.sender = {
+          name: ENV['SEND_IN_BLUE_SENDER_NAME'],
+          email: ENV['SEND_IN_BLUE_SENDER_EMAIL']
+        }
+        @sid_email.reply_to = {
+          email: ENV['SEND_IN_BLUE_REPLY_EMAIL'],
+          name: ENV['SEND_IN_BLUE_SENDER_NAME']
+        }
+
+        begin
+          @send_in_blue.send_transac_email(@sid_email)
+        rescue SibApiV3Sdk::ApiError => e
+          Rails.logger.error "Exception when calling TransactionalEmailsApi -> send_transac_email: #{e}"
+        end
+      else
+        mail(to: to, subject: subject)
+      end
     end
   end
 end
