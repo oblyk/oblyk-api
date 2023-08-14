e2faa85eb8e9b03dfa7f8f17a3a116ed2e6cd1ed
clucien <chastanlucien@gmail.com>
2023-08-14 10:48:10 +0200

    Correction problème de l'écart de cotation sur les sites avec des voies projets ou non côté

--- app/models/concerns/gap_gradable.rb
+++ app/models/concerns/gap_gradable.rb
@@ -7,8 +7,8 @@ module GapGradable
     gap_gradable = ENV.fetch('GAP_GRADABLE', 'false')
     return if gap_gradable == 'false'
 
-    max_grade = crag_routes.order(max_grade_value: :desc).first
-    min_grade = crag_routes.order(min_grade_value: :asc).first
+    max_grade = crag_routes.where('crag_routes.max_grade_value > 0').order(max_grade_value: :desc).first
+    min_grade = crag_routes.where('crag_routes.min_grade_value > 0').order(min_grade_value: :asc).first
 
     self.max_grade_value = max_grade&.max_grade_value
     self.max_grade_text = max_grade&.max_grade_text
