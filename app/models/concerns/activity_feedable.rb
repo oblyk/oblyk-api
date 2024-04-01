cec379bddb74288b6b4f51db8cc1e83957e93e14
clucien <chastanlucien@gmail.com>
2024-04-01 15:28:30 +0200

    On n'ajoute pas dans le feed les videos des voies indoor

--- app/models/concerns/activity_feedable.rb
+++ app/models/concerns/activity_feedable.rb
@@ -18,6 +18,7 @@ module ActivityFeedable
     return if instance_of?(AscentCragRoute) && %w[project repetition].include?(ascent_status)
     return if instance_of?(Article) && unpublished?
     return if instance_of?(Photo) && %w[Article Newsletter].include?(illustrable_type)
+    return if instance_of?(Video) && %w[GymRoute].include?(viewable_type)
 
     AddInFeedWorker.perform_in(10.seconds, self.class.name, id)
   end
