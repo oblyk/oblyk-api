ac2c9aa6fdc04fd398dd57cf45235d55e7c57215
clucien <chastanlucien@gmail.com>
2024-08-17 18:20:10 +0200

    Correction gym_team_user?

--- app/controllers/concerns/gymable.rb
+++ app/controllers/concerns/gymable.rb
@@ -19,6 +19,6 @@ module Gymable
   def protected_by_gym_administrator
     return if @current_user.super_admin
 
-    not_authorized unless gym_team_user
+    not_authorized unless gym_team_user?
   end
 end
