4b9e6be3abacaff1642f4b504386c46da0913821
clucien <chastanlucien@gmail.com>
2026-03-21 11:35:30 +0100

    Correction serialization

--- app/serializers/attachments_serializer_helper.rb
+++ app/serializers/attachments_serializer_helper.rb
@@ -3,12 +3,14 @@
 module AttachmentsSerializerHelper
   extend ActiveSupport::Concern
 
-  attribute :attachments do |object, params|
-    data = {}
-    type = class_name.gsub('Serializer', '').to_sym
-    params.fetch(:include_attachments, {})[type]&.each do |attachment_param|
-      data[attachment_param] = send("#{attachment_param}_attachment", object)
+  included do
+    attribute :attachments do |object, params|
+      data = {}
+      type = object.class.name.to_sym
+      params.fetch(:include_attachments, {})[type]&.each do |attachment_param|
+        data[attachment_param] = send("#{attachment_param}_attachment", object)
+      end
+      data
     end
-    data
   end
 end
